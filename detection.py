6757a467da7701f67c721f662d7bfec410e3d85f
mtm-x <mahasel.1969@gmail.com>
2025-02-17 01:17:23 +0530

    Refactor detection module to improve frame capture and processing; update ambulance detection functionality to save images with timestamps.

--- detection.py
+++ detection.py
@@ -6,87 +6,80 @@ from ultralytics import YOLO
 from scripts.ambulance import Ambulance
 
 model = YOLO("models/best.pt")
-FRAME_SKIP = 2
-frame_queue = queue.Queue(maxsize=2)
-display_frame_queue = queue.Queue(maxsize=2)
+
+FRAME_SKIP = 1  
+frame_queue = queue.Queue(maxsize=10)
+display_frame_queue = queue.Queue(maxsize=10)
 TARGET_WIDTH = 640
 TARGET_HEIGHT = 480
+stop_event = threading.Event()
 
 class detection():
-
     def __init__(self):
+        # self.video = cv2.VideoCapture("http://192.168.165.141:4747/video")
         self.video = cv2.VideoCapture("test_videos/ambulance.mp4")
+        if not self.video.isOpened():
+            print("Error: Could not open video stream. Please check your camera URL.")
+            return
+        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, TARGET_WIDTH)
+        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, TARGET_HEIGHT)
+        self.video.set(cv2.CAP_PROP_FPS, 30)
         print(model.names)
         self.ambulance = 0
-        self.stop_event = threading.Event()
 
-    def capture_frames(self,video):
-        frame_count = 0
-        while True:
+    def capture_frames(self, video):
+        while not stop_event.is_set():
             ret, frame = video.read()
             if not ret:
+                print("Failed to capture frame from video stream.")
                 break
-            frame_count += 1
-            if frame_count % FRAME_SKIP == 0:
-                if frame_queue.full():
-                    frame_queue.get()
-                frame_queue.put(frame)
-            else:
-                print("Failed to get frame from camera. Retrying...")
-                time.sleep(0.1)
-                
-    def detect_ambulance(self,frame):
-            results = model(frame, conf = 0.75)
-                # annotated_image = image.copy()  # Create a copy of the original frame for annotation
+            if frame_queue.full():
+                frame_queue.get()
+            frame_queue.put(frame)
+            print("Captured frame")
+            time.sleep(1 / 30)  # Sleep to maintain 30 FPS
 
-            for result in results:
-                for box in result.boxes:
-                    class_id = int(box.cls)  # Get class ID
-                    class_name = model.names[class_id]  # Get class name
-                    confidence = box.conf.item()  # Get confidence score
-                    if class_name :
-                            self.ambulance += 1
-                            print(self.ambulance)
-                            print(f"Detected: {class_name} (Confidence: {confidence:.2f})")
-                            annotated_image = results[0].plot()
-                            # # Plot the bounding box and label on the annotated image
-                            # c1, c2 = (int(box.xyxy[0][0]), int(box.xyxy[0][1])), (int(box.xyxy[0][2]), int(box.xyxy[0][3]))
-                            # cv2.rectangle(annotated_image, c1, c2, (0, 255, 0), 2)  # Green bounding box
-                            # cv2.putText(annotated_image, f'{class_name} {confidence:.2f}', (c1[0], c1[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+    def detect_ambulance(self, frame):
+        results = model(frame, conf=0.70)
+        annotated_image = frame.copy()  # Create a copy of the original frame for annotation
 
-                cv2.imshow("Image", annotated_image)
-            # if self.ambulance > 25:
-            #     Ambulance()
-            #     break
+        for result in results:
+            for box in result.boxes:
+                class_id = int(box.cls)  # Get class ID
+                class_name = model.names[class_id]  # Get class name
+                confidence = box.conf.item()  # Get confidence score
+                if class_name :
+                    self.ambulance += 1
+                    print(self.ambulance)
+                    print(f"Detected: {class_name} (Confidence: {confidence:.2f})")
+                    # Draw bounding box and label on the annotated image
+                    c1, c2 = (int(box.xyxy[0][0]), int(box.xyxy[0][1])), (int(box.xyxy[0][2]), int(box.xyxy[0][3]))
+                    cv2.rectangle(annotated_image, c1, c2, (0, 255, 0), 2)  # Green bounding box
+                    cv2.putText(annotated_image, f'{class_name} {confidence:.2f}', (c1[0], c1[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        cv2.imshow("Image", annotated_image)
+        if self.ambulance > 35:
+            Ambulance(frame=annotated_image)
+            self.ambulance = 0
             
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    self.stop_event.set()
-            return frame
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            stop_event.set()
+        return annotated_image
 
-    def process_frames(self,):
-        while not self.stop_event.is_set():
+    def process_frames(self):
+        while not stop_event.is_set():
             if not frame_queue.empty():
                 frame = frame_queue.get()
                 processed_frame = self.detect_ambulance(frame)
                 if display_frame_queue.full():
                     display_frame_queue.get()
                 display_frame_queue.put(processed_frame)
-            
+                print("Processed frame")
 
     def main(self):
-
-        video = cv2.VideoCapture("test_videos/ambulance.mp4")
-
-        if not video.isOpened():
-            print("Error: Could not open video stream. Please check your camera URL.")
-            return
-
-        # video.set(cv2.CAP_PROP_FRAME_WIDTH, TARGET_WIDTH)
-        # video.set(cv2.CAP_PROP_FRAME_HEIGHT, TARGET_HEIGHT)
-
         # Start threads
         threads = [
-            threading.Thread(target=self.capture_frames, args=(video,)),
+            threading.Thread(target=self.capture_frames, args=(self.video,)),
             threading.Thread(target=self.process_frames),
         ]
         
@@ -94,14 +87,14 @@ class detection():
             t.start()
 
         try:
-            while not self.stop_event.is_set():
+            while not stop_event.is_set():
                 time.sleep(0.1)
 
         except KeyboardInterrupt:
             print("Stopping...")
             
         finally:
-            self.stop_event.set()
+            stop_event.set()
             for t in threads:
                 t.join()
             self.video.release()
--- scripts/ambulance.py
+++ scripts/ambulance.py
@@ -1,2 +1,11 @@
-def Ambulance():
-    print("Ambulance is on the way")
+import cv2
+from datetime import datetime
+import os
+
+def Ambulance(frame):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"detections/ambulance_{timestamp}.jpg"
+    if not os.path.exists("detections"):
+        os.makedirs("detections")
+    cv2.imwrite(filename, frame)
+    return filename
